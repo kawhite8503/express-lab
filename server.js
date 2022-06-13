52d318c7944a0520e869ed754e1ec04d3b6040b6
Kathy White <kawhite8503@gmail.com>
2022-06-13 16:52:12 -0500

    add routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function(req,res){
   res.send('<h1>hello, friends</h1>')
 })
 
+app.get('/home', function (req, res){
+  res.send('<h1>Home Page</h1>')
+})
+
 //Tell app to listen on port 3000
 
 app.listen(3000, function(){
